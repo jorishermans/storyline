cd0f8cf6a4504b5aa60017558c0b7bb7bd1c8d80
joris hermans <JHermans85@hotmail.com>
2021-08-04 19:27:19 +0200

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <img alt="Origin logo" src="../assets/origin-logo.svg" />
+  <HelloWorld msg="origin-vue-ts" />
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
+
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
